19b932b1b0171e0641247cac6e9d578ec7ca5372
MicTome <michaeltomer@hotmail.com>
2016-05-09 18:41:42 +0200

    Fragment ingredientes(nevera) añadido. Lista de ingredientes agregada.

---
+++ app/src/main/java/fdi/ucm/thefridge/Ingrediente.java
@@ -0,0 +1,33 @@
+package fdi.ucm.thefridge;
+
+/**
+ * Created by Michael Tome Rodriguez on 02/05/2016.
+ * Clase de los ingredientes
+ */
+public class Ingrediente {
+    private String nombre;
+    private int img; //Las referencias a las imagenes se devuelven como enteros
+
+    public Ingrediente() {
+    }
+    public Ingrediente(String nombre, int img){
+        this.nombre = nombre;
+        this.img = img;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public int getImg(){
+        return img;
+    }
+
+    public void setImg(int img){
+        this.img = img;
+    }
+}
